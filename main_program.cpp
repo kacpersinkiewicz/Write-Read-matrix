d0aff8293cff72c31fafae7804662a30e7a0f877
kacpersinkiewicz <61428233+kacpersinkiewicz@users.noreply.github.com>
2020-03-03 13:47:14 +0100

    Update main_program.cpp

--- main_program.cpp
+++ main_program.cpp
@@ -14,36 +14,34 @@ void* read(void*)
 {
     while(1)
     {
-        int sleep_time;
         int input_s=1;
         int matrix[SIZE][SIZE];
-        cout << "r reader waiting for data acces" << endl;
+        cout << "[READER] Waiting before entering critical section" << endl;
         pthread_mutex_lock(&mutex);
-        cout << "r acces granted" << endl;
+        cout << "Entered critical section, reading data..." << endl;
         sleep(1);
         PDI_expose("input", &input_s, PDI_OUT); // set input to read
         PDI_expose("matrix_data", matrix, PDI_IN);
         pthread_mutex_unlock(&mutex);
-        cout << "r data unlocked" << endl;
+        cout << "[READER] Data read, leaving critical section" << endl;
 
-        for (int i = 0; i <5; i++) //reading data
+        for (int i = 0; i <SIZE; i++) //reading data
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < SIZE; j++)
             {
                 cout << matrix[i][j] << " ";
             }
             cout << endl;
         }
-        sleep_time = (( std::rand() % 3 ) + 1 ); //sleep for 1-3s
+        int sleep_time = (( std::rand() % 3 ) + 1 ); //sleep for 1-3s
         sleep(sleep_time);
-        cout << "r i was asleep for " << sleep_time << " seconds" << endl; 
+        cout << "[READER] Waiting for " << sleep_time << " seconds" << endl; 
     }
     return NULL;
 }
 
 void* write(void*)
 {
-    int sleep_time;
     int input_s=0;
     int matrix[SIZE][SIZE];
  
@@ -56,24 +54,24 @@ void* write(void*)
     }
     while(1)
     {
-        for (int l = 0; l<SIZE; l++)
+        for (int i = 0; l<SIZE; i++)
         {
-            for(int o=0; o<SIZE; o++)
+            for(int j=0; j<SIZE; j++)
             {
-                matrix[l][o]++;
+                matrix[i][j]++;
             }
         }
-        cout << "w writer waiting for data acces" << endl;
+        cout << "[WRITER] Waiting before entering critical section" << endl;
         pthread_mutex_lock(&mutex);
-        cout << "w acces granted" << endl;
+        cout << "[WRITER] Entered critical section, writing data..." << endl;
         sleep(5);
         PDI_expose("input", &input_s, PDI_OUT); // set input to write
         PDI_expose("matrix_data", matrix, PDI_OUT);
         pthread_mutex_unlock(&mutex);
-        cout << "w data unlocked" << endl;
-        sleep_time = (( std::rand() % 8 ) + 3 ); //sleep for 3-10s
+        cout << "[WRITER] Data written, leaving critical section" << endl;
+        int sleep_time = (( std::rand() % 8 ) + 3 ); //sleep for 3-10s
         sleep(sleep_time);
-        cout << "w i was asleep for " << sleep_time << " seconds" << endl; 
+        cout << "[WRITER] Waiting for " << sleep_time << " seconds" << endl; 
     }
     return NULL;
 }
