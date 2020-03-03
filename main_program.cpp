cfcc4e56901c9d870ed95f710e4c512474fdf569
kacpersinkiewicz <61428233+kacpersinkiewicz@users.noreply.github.com>
2020-03-03 13:22:15 +0100

    Update and rename test.cpp to main_program.cpp

--- main_program.cpp
+++ main_program.cpp
@@ -14,19 +14,19 @@ void* read(void*)
 {
     while(1)
     {
-        int zzz;
+        int sleep_time;
         int input_s=1;
         int matrix[SIZE][SIZE];
         cout << "r reader waiting for data acces" << endl;
         pthread_mutex_lock(&mutex);
         cout << "r acces granted" << endl;
         sleep(1);
-        PDI_expose("input", &input_s, PDI_OUT);
+        PDI_expose("input", &input_s, PDI_OUT); // set input to read
         PDI_expose("matrix_data", matrix, PDI_IN);
         pthread_mutex_unlock(&mutex);
         cout << "r data unlocked" << endl;
 
-        for (int i = 0; i <5; i++)
+        for (int i = 0; i <5; i++) //reading data
         {
             for (int j = 0; j < 5; j++)
             {
@@ -34,16 +34,16 @@ void* read(void*)
             }
             cout << endl;
         }
-        zzz = (( std::rand() % 3 ) + 1 );
-        sleep(zzz);
-        cout << "r i was asleep for " << zzz << " seconds" << endl; 
+        sleep_time = (( std::rand() % 3 ) + 1 ); //sleep for 1-3s
+        sleep(sleep_time);
+        cout << "r i was asleep for " << sleep_time << " seconds" << endl; 
     }
     return NULL;
 }
 
 void* write(void*)
 {
-    int zzz;
+    int sleep_time;
     int input_s=0;
     int matrix[SIZE][SIZE];
  
@@ -67,12 +67,12 @@ void* write(void*)
         pthread_mutex_lock(&mutex);
         cout << "w acces granted" << endl;
         sleep(5);
-        PDI_expose("input", &input_s, PDI_OUT);
+        PDI_expose("input", &input_s, PDI_OUT); // set input to write
         PDI_expose("matrix_data", matrix, PDI_OUT);
         pthread_mutex_unlock(&mutex);
         cout << "w data unlocked" << endl;
-        zzz = (( std::rand() % 8 ) + 3 );
-        sleep(zzz);
+        sleep_time = (( std::rand() % 8 ) + 3 ); //sleep for 3-10s
+        sleep(sleep_time);
         cout << "w i was asleep for " << zzz << " seconds" << endl; 
     }
     return NULL;
@@ -85,8 +85,8 @@ int main(int argc, char* argv[])
 {
     srand( time( NULL ) );
 
-    PDI_init(PC_parse_path("write.yml"));
-    int status = pthread_mutex_init(&mutex,NULL);
+    PDI_init(PC_parse_path("matrix_event.yml"));
+    int status = pthread_mutex_init(&mutex,NULL); //checking if mutex was implemented correctly
     if (status!=0)
     {
         cerr << "Error with creating a mutex" << endl;
@@ -97,7 +97,7 @@ int main(int argc, char* argv[])
 
     pthread_create(&writer, NULL, write, NULL); 
     pthread_create(&reader, NULL, read, NULL); 
-    pthread_join(writer, NULL);
+    pthread_join(writer, NULL); //prevents for killing threads in the end of main function
     pthread_join(reader, NULL);
     pthread_mutex_destroy(&mutex);
 
