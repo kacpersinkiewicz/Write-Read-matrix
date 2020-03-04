32527a03e197c00c6e367a4f35a372f614e9d535
kacpersinkiewicz <61428233+kacpersinkiewicz@users.noreply.github.com>
2020-03-04 11:03:31 +0100

    Update main_program.cpp

--- main_program.cpp
+++ main_program.cpp
@@ -50,58 +50,6 @@ void* write(void*)
     for (int i = 0; i < SIZE; i++)
     {
         for (int j = 0; j < SIZE; j++)
-#include <pdi.h>
-#include <iostream>
-#include <unistd.h>
-#include <pthread.h>
-#include <cstdlib>
-#include <ctime>
-#define SIZE 5
-
-
-using namespace std;
-pthread_mutex_t mutex;
-pthread_mutex_t mutex_reader;
-
-void* read(void*)
-{
-    pthread_mutex_lock (&mutex_reader);
-    while(1)
-    {
-        int input_s = 1;
-        int matrix[SIZE][SIZE];
-        cout << "[READER] Waiting before entering critical section" << endl;
-        pthread_mutex_lock(&mutex);
-        cout << "[READER] Entered critical section, reading data..." << endl;
-        sleep(1);
-        PDI_expose("input", &input_s, PDI_OUT); // set input to read
-        PDI_expose("matrix_data", matrix, PDI_IN);
-        pthread_mutex_unlock(&mutex);
-        cout << "[READER] Data read, leaving critical section" << endl;
-
-        for (int i = 0; i < SIZE; i++) //reading data
-        {
-            for (int j = 0; j < SIZE; j++)
-            {
-                cout << matrix[i][j] << " ";
-            }
-            cout << endl;
-        }
-        int sleep_time = (( std::rand() % 3 ) + 1 ); //sleep for 1 - 3s
-        sleep(sleep_time);
-        cout << "[READER] Waiting for " << sleep_time << " seconds" << endl; 
-    }
-    return NULL;
-}
-
-void* write(void*)
-{
-    int input_s = 0;
-    int matrix[SIZE][SIZE];
- 
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
         {
             matrix[i][j] = 0;
         }
