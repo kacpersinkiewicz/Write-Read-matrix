a992af0bd49a37ca82f905daad3253a6a9c6b5ee
kacpersinkiewicz <61428233+kacpersinkiewicz@users.noreply.github.com>
2020-03-04 13:30:33 +0100

    Update main_program.cpp

--- main_program.cpp
+++ main_program.cpp
@@ -11,26 +11,36 @@ bool work;
 
 using namespace std;
 
-pthread_mutex_t mutex;
-pthread_mutex_t mutex_reader;
+pthread_mutex_t mutex_data;
+pthread_mutex_t mutex_init_reader;
 pthread_mutex_t mutex_signal_reader;
 pthread_mutex_t mutex_signal_writer;
 
+void mask_sig(void)
+{
+    sigset_t mask;
+    sigemptyset(&mask);
+    sigaddset(&mask,SIGINT);
+    pthread_sigmask(SIG_BLOCK, &mask, NULL);
+}
+
 void* read(void*)
 {
-    pthread_mutex_lock (&mutex_reader);
+    mask_sig();
+    pthread_mutex_lock (&mutex_init_reader);
+    pthread_mutex_lock (&mutex_signal_reader); 
     while(work==true)
     {
         pthread_mutex_unlock (&mutex_signal_reader);
         int input_s = 1;
         int matrix[SIZE][SIZE];
         cout << "[READER] Waiting before entering critical section" << endl;
-        pthread_mutex_lock(&mutex);
+        pthread_mutex_lock(&mutex_data);
         cout << "[READER] Entered critical section, reading data..." << endl;
         sleep(1);
         PDI_expose("input", &input_s, PDI_OUT); // set input to read
         PDI_expose("matrix_data", matrix, PDI_IN);
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex_data);
         cout << "[READER] Data read, leaving critical section" << endl;
 
         for (int i = 0; i < SIZE; i++) //reading data
@@ -51,6 +61,7 @@ void* read(void*)
 
 void* write(void*)
 {
+    mask_sig();
     int input_s = 0;
     int matrix[SIZE][SIZE];
  
@@ -64,7 +75,8 @@ void* write(void*)
 
     PDI_expose ("input", &input_s, PDI_OUT);
     PDI_expose ("matrix_data", matrix, PDI_OUT);
-    pthread_mutex_unlock (&mutex_reader);
+    pthread_mutex_unlock (&mutex_init_reader);
+    pthread_mutex_lock (&mutex_signal_writer); 
 
     while (work==true)
     {
@@ -77,12 +89,12 @@ void* write(void*)
             }
         }
         cout << "[WRITER] Waiting before entering critical section" << endl;
-        pthread_mutex_lock (&mutex);
+        pthread_mutex_lock (&mutex_data);
         cout << "[WRITER] Entered critical section, writing data..." << endl;
         sleep (5);
         PDI_expose ("input", &input_s, PDI_OUT); // set input to write
         PDI_expose ("matrix_data", matrix, PDI_OUT);
-        pthread_mutex_unlock(&mutex);
+        pthread_mutex_unlock(&mutex_data);
         cout << "[WRITER] Data written, leaving critical section" << endl;
         int sleep_time = (( std::rand() % 8 ) + 3 ); //sleep for 3 - 10s
         sleep(sleep_time);
@@ -109,21 +121,21 @@ int main (int argc, char* argv[])
     signal(SIGINT, signalHandler);
 
     PDI_init (PC_parse_path("matrix_event.yml"));
-    int status = pthread_mutex_init (&mutex, NULL); //checking if mutex was implemented correctly
+    int status = pthread_mutex_init (&mutex_data, NULL); //checking if mutex was implemented correctly
     if (status != 0)
     {
         cerr << "Error with creating a mutex" << endl;
         return status; 
     }
     
-    int status_reader = pthread_mutex_init (&mutex_reader, NULL);   
+    int status_reader = pthread_mutex_init (&mutex_init_reader, NULL);   
     if (status_reader != 0)
     {
         cerr << "Error with creating a mutex" << endl;
         return status; 
     }
     
-    pthread_mutex_lock (&mutex_reader);
+    pthread_mutex_lock (&mutex_init_reader);
 
     pthread_t writer;
     pthread_t reader;
@@ -132,8 +144,8 @@ int main (int argc, char* argv[])
     pthread_create (&reader, NULL, read, NULL); 
     pthread_join (writer, NULL); //prevents for killing threads in the end of main function
     pthread_join (reader, NULL);
-    pthread_mutex_destroy (&mutex);
-    pthread_mutex_destroy (&mutex_reader);
+    pthread_mutex_destroy (&mutex_data);
+    pthread_mutex_destroy (&mutex_init_reader);
     pthread_mutex_destroy (&mutex_signal_reader);
     pthread_mutex_destroy (&mutex_signal_writer);
 
